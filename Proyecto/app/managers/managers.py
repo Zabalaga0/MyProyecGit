caa23a6da2a0c8c4f02819f35ac2a3463b332e89
Zabalaga0 <alancristhianzabalagayucra@gmail.com>
2025-09-17 01:52:52 -0400

    Logica de negocio(Manager)

--- Proyecto/app/managers/managers.py
+++ Proyecto/app/managers/managers.py
@@ -1,1 +1,35 @@
 # Aquí va la lógica de negocio
+from sqlalchemy.orm import Session
+from app.models.product import Product
+from app.schemas.product_schema import ProductCreate
+
+def get_products(db: Session):
+    return db.query(Product).all()
+
+def get_product_by_id(db: Session, product_id: int):
+    return db.query(Product).filter(Product.id == product_id).first()
+
+def create_product(db: Session, product: ProductCreate):
+    new_product = Product(**product.dict())
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
+
+def update_product(db: Session, product_id: int, updated_data: ProductCreate):
+    product = get_product_by_id(db, product_id)
+    if not product:
+        return None
+    for key, value in updated_data.dict().items():
+        setattr(product, key, value)
+    db.commit()
+    db.refresh(product)
+    return product
+
+def delete_product(db: Session, product_id: int):
+    product = get_product_by_id(db, product_id)
+    if not product:
+        return None
+    db.delete(product)
+    db.commit()
+    return product
