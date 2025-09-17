a9bc8b01ca4b173f42b27ef6a3ce1133713d5f92
Zabalaga0 <alancristhianzabalagayucra@gmail.com>
2025-09-17 01:57:45 -0400

    Archivo Principal de FastAPI

--- Proyecto/app/main.py
+++ Proyecto/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from app.db.connection import Base, engine
+from app.interfaces import product_routes
 
-app = FastAPI()
+# Crear las tablas si no existen
+Base.metadata.create_all(bind=engine)
 
-@app.get('/hello')
-def read_root():
-    return {'message': 'Hola Mundo '}
+app = FastAPI(title="Sistema de Gestión de Productos")
+
+# Incluir rutas
+app.include_router(product_routes.router)
+
+@app.get("/")
+def root():
+    return {"message": "Bienvenido a la API de gestión de productos"}
