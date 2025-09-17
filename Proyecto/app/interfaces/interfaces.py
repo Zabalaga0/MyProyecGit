184b4825ba41df77efcd5b39483d16e97307c048
Zabalaga0 <alancristhianzabalagayucra@gmail.com>
2025-09-17 01:54:49 -0400

    Rutas de la API(FastAPI Endpoints)

--- Proyecto/app/interfaces/interfaces.py
+++ Proyecto/app/interfaces/interfaces.py
@@ -1,1 +1,37 @@
 # Aquí van las rutas de FastAPI
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.connection import get_db
+from app.schemas.product_schema import ProductCreate, ProductResponse
+from app.managers import product_manager
+
+router = APIRouter(prefix="/products", tags=["Products"])
+
+@router.get("/", response_model=list[ProductResponse])
+def list_products(db: Session = Depends(get_db)):
+    return product_manager.get_products(db)
+
+@router.post("/", response_model=ProductResponse)
+def create_new_product(product: ProductCreate, db: Session = Depends(get_db)):
+    return product_manager.create_product(db, product)
+
+@router.get("/{product_id}", response_model=ProductResponse)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    product = product_manager.get_product_by_id(db, product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+@router.put("/{product_id}", response_model=ProductResponse)
+def update_product(product_id: int, product: ProductCreate, db: Session = Depends(get_db)):
+    updated_product = product_manager.update_product(db, product_id, product)
+    if not updated_product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return updated_product
+
+@router.delete("/{product_id}", response_model=ProductResponse)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    deleted_product = product_manager.delete_product(db, product_id)
+    if not deleted_product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return deleted_product
