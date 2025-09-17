66d575f519fd2c5740ee6a2ccdbffe581d313fb6
Zabalaga0 <alancristhianzabalagayucra@gmail.com>
2025-09-17 01:49:42 -0400

    Añadiendo codigo a los modelos, modelo de ejemplo producto

--- Proyecto/app/models/models.py
+++ Proyecto/app/models/models.py
@@ -1,1 +1,12 @@
 # Aquí van los modelos (tablas SQLAlchemy)
+from sqlalchemy import Column, Integer, String, Float
+from app.db.connection import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    brand = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    stock = Column(Integer, nullable=False, default=0)
